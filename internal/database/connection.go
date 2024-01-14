46f4a59ec93832a4ce8dba9cb16ea7569182bd53
Luizhnrs <Luizhnrsg@gmail.com>
2024-01-14 15:31:20 -0300

    Feat: Connection

---
+++ internal/database/connection.go
@@ -0,0 +1,30 @@
+package database
+
+import (
+	"context"
+	"fmt"
+	"time"
+
+	"github.com/exaring/otelpgx"
+)
+
+var Conn *pgxpool.Pool
+
+func NewConnection(ctx context.Context, connectionString string) (*pgxpool.Pool, error) {
+	ctx, cancel := context.WithTimeout(ctx, 10*time.Second)
+	defer cancel()
+
+	cfg, err := pgxpool.ParseConfig(connectionString)
+	if err != nil {
+		return nil, fmt.Errorf("create connection pool: %w", err)
+	}
+
+	cfg.ConnConfig.Tracer = otelpgx.NewTracer()
+
+	Conn, err = pgxpool.NewWithConfig(ctx, cfg)
+	if err != nil {
+		return nil, err
+	}
+
+	return Conn, nil
+}
